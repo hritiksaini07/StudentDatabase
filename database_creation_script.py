15e0cab5af38574b3e0a5f3a19cb704a6b0a0362
HRITIK SAINI <69029275+hritiksaini07@users.noreply.github.com>
2021-01-29 10:13:23 +0530

    Add files via upload

---
+++ database_creation_script.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+sqliteConnection=sqlite3.connect('student_database.db')
+cursor=sqliteConnection.cursor()
+print("Database created and connected successfully")
+
+try:
+    sqlite_create_table_query='''CREATE TABLE record(name TEXT, roll INTEGER, course TEXT, age INTEGER,dob TEXT);'''
+
+
+    cursor.execute(sqlite_create_table_query)
+    sqliteConnection.commit()
+    print("Table is created")
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Error is:-",error)
+    
